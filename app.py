21bb745550163be59ba4606624ae94f97ce5a96d
Ryzen <grille299@gmail.com>
2025-03-31 18:39:09 +0300

    ваиfg

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from flask_migrate import Migrate
+from sqlalchemy import func, asc, desc
 from db import *
 from forms import *
 from UserLogin import UserLogin
@@ -40,6 +41,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
 login_manager.login_message_category = "success"
+
+GENRES = ('🔫Экшн', '🌏Приключения', '🧙‍♂️RPG', '📈Стратегия', '💼Симулятор', '⚽Спорт', '🗿Головоломка', '🏃‍♂️Платформер',  'Другое')
 #-----------------------------------------------------------------------------------------------------------------
 
 """
@@ -98,29 +101,7 @@ def index():
         flash(f"Ошибка получения списка игр: {str(e)}", "error")
         games = []
     return render_template('index.html', title="Игровой развелекательный портал", menu=menu, user=current_user, games=games)
-#-----------------------------------------------------------------------------------------------------------------
-"""
-                                             Маршрут для ИГР Pygame
-"""
-#-----------------------------------------------------------------------------------------------------------------
 
-@app.route('/unity')
-@login_required
-def unity():
-    game_path = f'games/{request.cookies.get("game_path")}/index.html'
-    return send_from_directory(os.path.join(app.static_folder), game_path)
-
-@app.route('/pygame')
-@login_required
-def pygame():
-    game_path = f'games/{request.cookies.get("game_path") }/build/web'
-    return send_from_directory(os.path.join(app.static_folder, game_path), 'index.html')
-
-
-@app.route('/<path:path>')
-@login_required
-def game_static_files(path):
-    return send_from_directory(os.path.join(app.static_folder, f'games/{path.removesuffix(".apk")}/build/web'), path)
 #-----------------------------------------------------------------------------------------------------------------
 """
                                       Маршрут страницы СПИСКА ИГР на сайте
@@ -131,11 +112,42 @@ def game_static_files(path):
 def listgames():
     menu = MainMenu.query.all()
     try:
-        games = Games.query.all()
+        # Получаем параметры запроса
+        search = request.args.get('search', '').strip()
+        sort = request.args.get('sort_name', 'time_desc')  # По умолчанию сортировка по дате убывания
+        filter_type = request.args.get('type_filter', '')  # Фильтр по типу игры
+        filter_genre = request.args.get('genre_filter', '')  # Фильтр по жанру
+        # Базовый запрос
+        query = Games.query
+        # Поиск по названию или описанию
+        if search:
+            query = query.filter(
+                (Games.title.ilike(f'%{search}%')) |
+                (Games.description.ilike(f'%{search}%'))
+            )
+        # Фильтрация по типу игры
+        if filter_type:
+            query = query.filter(Games.type == filter_type)
+        # Сортировка
+        if filter_genre:
+            query = query.filter(Games.genre == filter_genre)
+        if sort == 'title_asc':
+            query = query.order_by(asc(Games.title))
+        elif sort == 'title_desc':
+            query = query.order_by(desc(Games.title))
+        elif sort == 'time_asc':
+            query = query.order_by(asc(Games.time))
+        elif sort == 'time_desc':
+            query = query.order_by(desc(Games.time))
+        else:
+            query = query.order_by(desc(Games.time))  # По умолчанию
+
+        games = query.all()
     except Exception as e:
-        flash(f"Ошибка получения списка игр: {str(e)}", "error")
+        flash(f'Ошибка получения списка игр: {str(e)}', 'error')
         games = []
-    return render_template('listgames.html', title="Игры", menu=menu, games=games)
+    return render_template('listgames.html', title="Игры", menu=menu, games=games,
+                          search=search, sort=sort, filter_type=filter_type, filter_genre=filter_genre, genres=GENRES)
 #-----------------------------------------------------------------------------------------------------------------
 """
                                       Маршрут страницы ИГРЫ на сайте
@@ -148,14 +160,30 @@ def game(game_id):
     menu = MainMenu.query.all()
 
     response = make_response(render_template('game.html', menu=menu, title=game.title, game=game))
-    if game.game_type == 'link':
+    if game.type == 'link':
         response.set_cookie('game_path', '', path='/', samesite='Lax')  # Нет пути для внешних ссылок
-    elif game.game_type == 'pygame':
+    elif game.type == 'pygame':
         response.set_cookie('game_path', game.link, path='/', samesite='Lax')
-    elif game.game_type == 'unity':
+    elif game.type == 'unity':
         response.set_cookie('game_path', game.link, path='/', samesite='Lax')
 
     return response
+
+#-----------------------------------------------------------------------------------------------------------------
+"""
+                                             Маршрут для ИГР Pygame
+"""
+#-----------------------------------------------------------------------------------------------------------------
+@app.route('/pygame')
+@login_required
+def pygame():
+    game_path = f'games/{request.cookies.get("game_path")}/build/web'
+    return send_from_directory(os.path.join(app.static_folder, game_path), 'index.html')
+
+@app.route('/<path:path>')
+@login_required
+def game_static_files(path):
+    return send_from_directory(os.path.join(app.static_folder, f'games/{path.removesuffix(".apk")}/build/web'), path)
 #-----------------------------------------------------------------------------------------------------------------
 """
                                     Маршрут для скачивания УСТАНОВЩИКА ИГРЫ на сайте
@@ -168,7 +196,6 @@ def download_installer(game_id):
     if game.installer and os.path.exists(game.installer):
         return send_file(game.installer, as_attachment=True, download_name=f"{game.title}.exe")
     abort(404)
-
 #-----------------------------------------------------------------------------------------------------------------
 """
                                     Маршрут страницы АВТОРИЗАЦИИ на сайте
--- db.py
+++ db.py
@@ -106,8 +106,8 @@ class Games(db.Model):
     cover = db.Column(db.LargeBinary, nullable=False)  # Ссылка на обложку
     link = db.Column(db.Text, nullable=False)  # HTML ссылка на игру
     genre = db.Column(db.Text, nullable=True)  # Жанр
+    type = db.Column(db.String(20), nullable=False, default='link')  # Тип игры: 'link', 'pygame', 'unity'
     installer = db.Column(db.Text, nullable=True)  # Путь к установщику
-    game_type = db.Column(db.String(20), nullable=False, default="link")
     time = db.Column(db.Integer, nullable=False, default=datetime.utcnow)  # Добавлено поле времени
 
     def __repr__(self):
